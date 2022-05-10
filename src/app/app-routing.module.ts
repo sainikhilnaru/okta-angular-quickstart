3080973acab0ff0e4f9ad4246bceb801cdab99a4
sainikhilnaru <sainikhilnaru@gmail.com>
2022-05-10 13:30:14 -0500

    OIDC works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OktaAuthGuard, OktaCallbackComponent } from '@okta/okta-angular';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [{ path: 'login/callback', component: OktaCallbackComponent },
+{
+  path: 'protected',
+  loadChildren: () => import('./protected/protected.module').then(m => m.ProtectedModule),
+  canActivate: [OktaAuthGuard]
+},
+{ path: 'profile', component: ProfileComponent, canActivate: [OktaAuthGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
+import { OktaAuthModule, OKTA_CONFIG } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './auth.interceptor';
+
+const oktaAuth = new OktaAuth({
+  issuer: 'https://dev-10749791.okta.com/oauth2/default',
+  clientId: '0oa4x1n2izGx7PBn25d7',
+  redirectUri: window.location.origin + '/login/callback'
+});
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    OktaAuthModule
+    // ,OktaAuth
   ],
-  providers: [],
+
+  providers: [{ provide: OKTA_CONFIG, useValue: { oktaAuth } },
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
